a733c8eb55e9887bfe64d1ca4489d4b22ac2e948
Maryorie lizzette <maaryorielizzette-10@outlook.es>
2022-03-17 22:31:13 +0100

    nuevo control

--- Ejercicios-UtilizandoGitHub/Ejercicios-UtilizandoGitHub/Form1.Designer.cs
+++ Ejercicios-UtilizandoGitHub/Ejercicios-UtilizandoGitHub/Form1.Designer.cs
@@ -31,6 +31,7 @@ namespace Ejercicios_UtilizandoGitHub
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // button1
@@ -52,16 +53,28 @@ namespace Ejercicios_UtilizandoGitHub
             this.button2.Text = "Recorrer Vector";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(356, 100);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 21);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(361, 270);
+            this.ClientSize = new System.Drawing.Size(599, 360);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +82,7 @@ namespace Ejercicios_UtilizandoGitHub
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
